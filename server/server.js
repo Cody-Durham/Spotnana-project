7355c8a29270257d7684b2c17283d0683cdcc67d
codydurham <codydd@gmail.com>
2026-04-18 00:19:28 -0600

    small notes change

--- server/server.js
+++ server/server.js
@@ -13,44 +13,16 @@ const nasaKey = process.env.NASA_API_KEY;
 
 app.use(cors(corsOptions));
 
-// Backend API route for Nasa picture of the day
-// app.get("/api/pic", async (req, res) => {
-//   const date = new Date();
-//   date.setDate(date.getDate() - 1);
-
-//   const result = date.toISOString().split("T")[0];
-//   const haveDate = result ? result : "2026-04-13"; // "2026-04-13" is a beautiful image for default
-
-
-
-//   try {
-//     const response = await axios.get("https://api.nasa.gov/planetary/apod", {
-//     params: {
-//       api_key: nasaKey,
-//       // date: haveDate
-//       date: "2026-04-13"
-//     }
-//   }
-// );
-//     const pic = response.data;
-//     res.json(pic) ;
-
-//   } catch (err) {
-//     console.error("ERROR:", err.message);
-//     res.status(500).json({ error: "Failed to fetch data" });
-//   }
-// });
-
-
 const getYesterdayDate = () => {
   const date = new Date();
   date.setDate(date.getDate() -1 );
   return date.toISOString().split("T")[0];
-}
+};
 
+// Backend API route from NASA picture of the day
 app.get("/api/pic", async (req, res) => {
   try {
-    // check for nasa key
+    // check for nasa key. if no key send 500 (server error)
     if (!nasaKey) {
       // ******************* I should put up a dialog here *****************
       return res.status(500).json({ error: "NASA API key is missing" });
@@ -67,6 +39,7 @@ app.get("/api/pic", async (req, res) => {
       }
     );
 
+    // if the upstream server sends a invalid response
     if (!response.data || !response.data.url) {
       // ******************* I should put up a dialog here *****************
       return res.status(502).json({
