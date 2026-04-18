b77abd50885b65bd4cc51b94a0b397fde158cf58
codydurham <codydd@gmail.com>
2026-04-17 22:59:07 -0600

    add new artwork assets. install dotevn. add .env file. add .gitignore file.  move nasa api key over to .env file.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const axios = require("axios");
+
 const corsOptions = {
     origin: ["http://localhost:5173"]
 };
-const axios = require("axios");
-const nasaKey = "A2duc4qlrKHp7ptCSu8KYAG6148GE7jgzNvM7Xcj"
+
+const nasaKey = process.env.NASA_API_KEY;
+console.log("nasaKey!!!!!!!!!!!!!!!", nasaKey);
+
 
 app.use(cors(corsOptions));
 
@@ -28,7 +34,7 @@ app.get("/api/pic", async (req, res) => {
       date: "2026-04-13"
     }
   }
-);1``
+);
     const pic = response.data;
     res.json(pic) ;
 
@@ -37,21 +43,6 @@ app.get("/api/pic", async (req, res) => {
     res.status(500).json({ error: "Failed to fetch data" });
   }
 });
-
-// Backend API route for test asteriod data
-// app.get("/api/close", async (req, res) => {
-//   try {
-//     // const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${nasaKey}`);
-//     const response = await axios.get(`https://api.nasa.gov/EPIC/api/natural/date/2019-05-30?api_key=${nasaKey}`);
-//     console.log("hit data!!!!!!!!!", response);
-    
-//     const data = response.data;
-//     res.json(data);
-//   } catch (err) {
-//     res.status(500).json({error: "Faild to fetch data"});
-//   }
-// });
-
 
 app.listen(8080, () => {
   console.log("Server running on 8080");
